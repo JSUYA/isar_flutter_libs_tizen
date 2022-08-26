b618d181781e18ded392c146cf21ac5febda5f61
Swift Kim <swift.kim@samsung.com>
2022-08-26 11:29:57 +0900

    Simplify

--- tizen/src/isar_flutter_libs_tizen_plugin.cc
+++ tizen/src/isar_flutter_libs_tizen_plugin.cc
@@ -1,51 +1,6 @@
 #include "isar_flutter_libs_tizen_plugin.h"
 
-#include <flutter/method_channel.h>
 #include <flutter/plugin_registrar.h>
-#include <flutter/standard_method_codec.h>
-
-#include <memory>
-#include <string>
-
-#include "log.h"
-
-namespace {
-
-class IsarFlutterLibsTizenPlugin : public flutter::Plugin {
- public:
-  static void RegisterWithRegistrar(flutter::PluginRegistrar *registrar) {
-    auto channel =
-        std::make_unique<flutter::MethodChannel<flutter::EncodableValue>>(
-            registrar->messenger(), "isar_flutter_libs_tizen",
-            &flutter::StandardMethodCodec::GetInstance());
-
-    auto plugin = std::make_unique<IsarFlutterLibsTizenPlugin>();
-
-    channel->SetMethodCallHandler(
-        [plugin_pointer = plugin.get()](const auto &call, auto result) {
-          plugin_pointer->HandleMethodCall(call, std::move(result));
-        });
-
-    registrar->AddPlugin(std::move(plugin));
-  }
-
-  IsarFlutterLibsTizenPlugin() {}
-
-  virtual ~IsarFlutterLibsTizenPlugin() {}
-
- private:
-  void HandleMethodCall(
-      const flutter::MethodCall<flutter::EncodableValue> &method_call,
-      std::unique_ptr<flutter::MethodResult<flutter::EncodableValue>> result) {
-    result->NotImplemented();
-  }
-};
-
-}  // namespace
 
 void IsarFlutterLibsTizenPluginRegisterWithRegistrar(
-    FlutterDesktopPluginRegistrarRef registrar) {
-  IsarFlutterLibsTizenPlugin::RegisterWithRegistrar(
-      flutter::PluginRegistrarManager::GetInstance()
-          ->GetRegistrar<flutter::PluginRegistrar>(registrar));
-}
+    FlutterDesktopPluginRegistrarRef registrar) {}
